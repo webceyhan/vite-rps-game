e9154d4b04eeb5f5e795beb659652aaafcbb8799
Evren Ceyhan <evren@ceyhan.io>
2022-01-23 02:28:58 +0300

    rps icons added

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,32 @@
 <template>
   <main class="p-5">
     <div class="container text-center">
-      <h1 class="display-1">Rock Paper Scissors</h1>
+      <h1 class="display-1 mb-5">Rock Paper Scissors</h1>
+
+      <div class="row justify-content-center">
+        <div class="col-3">
+          <a href="#" class="btn btn-primary btn-img rounded-circle p-2">
+            <img src="./assets/rock.png" class="img-fluid rounded-circle" />
+          </a>
+        </div>
+
+        <div class="col-3">
+          <a href="#" class="btn btn-primary btn-img rounded-circle p-2">
+            <img src="./assets/paper.png" class="img-fluid" />
+          </a>
+        </div>
+        <div class="col-3">
+          <a href="#" class="btn btn-primary btn-img rounded-circle p-2">
+            <img src="./assets/scissors.png" class="img-fluid" />
+          </a>
+        </div>
+      </div>
     </div>
   </main>
 </template>
+
+<style>
+.btn-img:hover > img {
+  filter: opacity(0.9);
+}
+</style>
