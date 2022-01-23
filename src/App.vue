f296713aa8ae4d3b2b8c8fab85596daee734d166
Evren Ceyhan <evren@ceyhan.io>
2022-01-23 03:13:18 +0300

    player cards added

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,45 @@
 
 <template>
   <main class="p-5">
-    <div class="container text-center">
-      <h1 class="display-1 mb-5">Rock Paper Scissors</h1>
+    <div class="container-fluid text-center">
+      <h1 class="display-1">Rock Paper Scissors</h1>
 
-      <div class="row justify-content-center">
+      <hr class="mb-5" />
+
+      <div class="row align-items-center justify-content-between">
+        <!-- user score -->
         <div class="col-3">
-          <a href="#" class="btn btn-primary btn-img rounded-circle p-2">
+          <div class="card bg-secondary text-white">
+            <div class="card-body">
+              <h4>You</h4>
+              <p class="display-3 m-0">1</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- game buttons -->
+        <div class="col-6 d-flex">
+          <a href="#" class="btn btn-primary btn-img rounded-circle p-2 mx-2">
             <img src="./assets/rock.png" class="img-fluid rounded-circle" />
           </a>
-        </div>
 
-        <div class="col-3">
-          <a href="#" class="btn btn-primary btn-img rounded-circle p-2">
+          <a href="#" class="btn btn-primary btn-img rounded-circle p-2 mx-2">
             <img src="./assets/paper.png" class="img-fluid" />
           </a>
-        </div>
-        <div class="col-3">
-          <a href="#" class="btn btn-primary btn-img rounded-circle p-2">
+
+          <a href="#" class="btn btn-primary btn-img rounded-circle p-2 mx-2">
             <img src="./assets/scissors.png" class="img-fluid" />
           </a>
+        </div>
+
+        <!-- computer score -->
+        <div class="col-3">
+          <div class="card bg-secondary text-white">
+            <div class="card-body">
+              <h4>Computer</h4>
+              <p class="display-3 m-0">1</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
