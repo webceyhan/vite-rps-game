726a0f70a60653c0bd83330b6b4309bdd0979e8b
Evren Ceyhan <evren@ceyhan.io>
2022-01-23 15:38:09 +0300

    game logic impl.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,67 @@
 <script setup lang="ts">
 
+import { ref } from 'vue'
+
+interface RuleMap {
+  [key: string]: { [key: string]: number }
+}
+
+const rules: RuleMap = {
+  // rock -> scissor -> paper
+  rock: { scissor: 1, paper: -1 },
+  scissor: { paper: 1, rock: -1 },
+  paper: { rock: 1, scissor: -1 },
+}
+
+const moves = Object.keys(rules);
+
+const randomMove = () => moves[Math.floor(Math.random() * moves.length)]
+
+const compareMoves = (a: string, b: string) => {
+  return rules[a][b] || 0;
+}
+
+const message = ref('start playing..')
+
+const user = ref({
+  score: 0,
+  move: ''
+});
+
+const computer = ref({
+  score: 0,
+  move: ''
+})
+
+
+function onChoose(move: string) {
+  user.value.move = move;
+  computer.value.move = randomMove();
+
+  const result = compareMoves(move, computer.value.move);
+
+  switch (result) {
+    case 1:
+      user.value.score += 1;
+      message.value = 'You won!';
+      break;
+
+    case -1:
+      computer.value.score += 1;
+      message.value = 'You lost!'
+      break;
+
+    default:
+      message.value = "It's a TIE!"
+      break;
+  }
+
+}
+
 </script>
 
 <template>
-  <main class="p-5">
+  <main class="py-5 p-md-5">
     <div class="container-fluid text-center">
       <h1 class="display-1">Rock Paper Scissors</h1>
 
@@ -11,39 +69,42 @@
 
       <div class="row align-items-center justify-content-between">
         <!-- user score -->
-        <div class="col-3">
+        <div class="col-6 col-md-3 order-md-1">
           <div class="card bg-secondary text-white">
             <div class="card-body">
               <h4>You</h4>
-              <p class="display-3 m-0">1</p>
+              <p class="display-3">{{ user.score }}</p>
+              <p class="text-capitalize bg-dark m-0">{{ user.move }}</p>
             </div>
           </div>
         </div>
 
         <!-- game buttons -->
-        <div class="col-6 d-flex">
-          <a href="#" class="btn btn-primary btn-img rounded-circle p-2 mx-2">
-            <img src="./assets/rock.png" class="img-fluid rounded-circle" />
-          </a>
-
-          <a href="#" class="btn btn-primary btn-img rounded-circle p-2 mx-2">
-            <img src="./assets/paper.png" class="img-fluid" />
-          </a>
-
-          <a href="#" class="btn btn-primary btn-img rounded-circle p-2 mx-2">
-            <img src="./assets/scissors.png" class="img-fluid" />
-          </a>
+        <div class="col-12 col-md-6 d-flex order-1 order-md-2 py-5">
+          <button
+            v-for="move in moves"
+            :key="move"
+            @click="onChoose(move)"
+            class="btn btn-primary btn-img rounded-circle p-2 mx-2"
+          >
+            <img :src="`/src/assets/${move}.png`" class="img-fluid rounded-circle" />
+          </button>
         </div>
 
         <!-- computer score -->
-        <div class="col-3">
+        <div class="col-6 col-md-3 order-md-3">
           <div class="card bg-secondary text-white">
             <div class="card-body">
               <h4>Computer</h4>
-              <p class="display-3 m-0">1</p>
+              <p class="display-3">{{ computer.score }}</p>
+              <p class="text-capitalize bg-dark m-0">{{ computer.move }}</p>
             </div>
           </div>
         </div>
+      </div>
+
+      <div class="row justify-content-between">
+        <h1 class="display-5">{{ message }}</h1>
       </div>
     </div>
   </main>
