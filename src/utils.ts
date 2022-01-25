b3b297d0eba9d4a08ea61194a37b4a3a15faf475
Evren Ceyhan <evren@ceyhan.io>
2022-01-25 14:38:46 +0300

    bugfix: unused param

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,7 @@ export const compare = (a: number, b: number) => (a > b ? 1 : a < b ? -1 : 0);
 export const delay = (fn: Function, ms = 1000) => setTimeout(fn, ms);
 
 export const delayAsync = async (ms = 1000) => {
-    return new Promise((resolve) => delay(resolve));
+    return new Promise((resolve) => delay(resolve, ms));
 };
 
 export const rootUrl = (path: string) =>
