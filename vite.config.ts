e763fb32703722e39764ad1683bedbe51e85ada8
Evren Ceyhan <evren@ceyhan.io>
2022-01-26 19:16:29 +0300

    quick fix for broken-link on production

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'./', // fix for github-pages
   plugins: [vue()],
   css: { preprocessorOptions: { scss: { charset: false } } }
 })
